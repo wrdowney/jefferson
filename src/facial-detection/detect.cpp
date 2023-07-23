c93c19e8acb2bbbed961c929030dcbef907e2556
wrdowney <willd20@vt.edu>
2023-07-23 19:19:52 -0400

    Basic facial detection

---
+++ src/facial-detection/detect.cpp
@@ -0,0 +1,42 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#include <string>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main (int argc, const char *argv[])
+{
+    Mat video_stream;
+    VideoCapture cap(0);
+    namedWindow("Facial Detection");
+
+    string classifier_loc = "/usr/share/opencv4/haarcascades/haarcascade_frontalface_alt.xml"; 
+    CascadeClassifier face_cascade;
+    face_cascade.load(classifier_loc);
+
+    vector<Rect> faces;
+    while (true)
+    {
+        face_cascade.detectMultiScale(video_stream, faces, 1.1, 4, CASCADE_SCALE_IMAGE, Size(30, 30));
+        cap.read(video_stream);
+        
+        if (faces.empty())
+        {
+            cout << "No faces detected" << endl;
+        }
+
+        for (Rect face : faces)
+        {
+            Mat faceROI = video_stream(face);
+            rectangle(video_stream, face, Scalar(255, 0, 0), 2, 8, 0);
+        }
+
+        imshow("Facial Detection", video_stream);
+        if (waitKey(10) == 27) break;
+    }
+    return 0;
+}
